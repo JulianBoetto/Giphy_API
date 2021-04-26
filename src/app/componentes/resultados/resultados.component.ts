5f10a0563b9312f0f7f56b42cf966674acfe350b
JulianBoetto <julib_8724@hotmail.com>
2021-04-26 20:17:17 -0300

    src pronto. Imagens prontas

--- src/app/componentes/resultados/resultados.component.ts
+++ src/app/componentes/resultados/resultados.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ResultadosService } from 'src/app/servicos/resultados.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ResultadosService } from "../../servicos/resultados.service";
 
 @Component({
-  selector: 'app-resultados',
-  templateUrl: './resultados.component.html',
-  styleUrls: ['./resultados.component.css']
+  selector: "app-resultados",
+  templateUrl: "./resultados.component.html",
+  styleUrls: ["./resultados.component.css"]
 })
 export class ResultadosComponent implements OnInit {
-
   gifs: any = [];
   pesquisa!: string;
-  
-  constructor(private resultadosServ: ResultadosService, private route: ActivatedRoute) { }
+
+  constructor(
+    private resultadosServ: ResultadosService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    
     this.route.params.subscribe(params => {
-      this.pesquisa = params['palabra'];
-      
+      this.pesquisa = params["palabra"];
+
       this.resultadosServ.getResultados(this.pesquisa).subscribe(gifs => {
-      this.gifs = gifs;
+        this.gifs = gifs;
       });
     });
   }
-
 }
